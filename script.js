dc5c632726fe28d5e9470b209c7346b09645db5e
Aditi-P22 <patil22aditi@gmail.com>
2025-05-28 19:11:39 +0530

    added home page

---
+++ script.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}", // Adjust this path if your files are elsewhere
+  ],
+  theme: {
+    extend: {
+      animation: {
+        "marquee-slow": "marquee 30s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
